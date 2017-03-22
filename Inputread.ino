2a00af7f366beee85cac5eb3774c13a8efb172f0
nacho-rz <ignat.m.georgiev@gmail.com>
2017-03-22 22:22:38 +0000

    how bout dat

--- Inputread.ino
+++ Inputread.ino
@@ -1,54 +1,125 @@
-int pinread;
-int highsig = 0;
-int lowsig = 0;
-int period = 0;
-unsigned long currenttime = 0;
-unsigned long previoustime = 0;
-unsigned long t;
+// these are checked for in the main program
+volatile unsigned long timerCounts;
+volatile boolean counterReady;
+
+// internal to counting routine
+unsigned long overflowCount;
+unsigned int timerTicks;
+unsigned int timerPeriod;
+
+// Input: Pin D5
 
 void setup() {
-  // put your setup code here, to run once:
-pinMode(2, INPUT);
-pinMode(3, OUTPUT);
-Serial.begin(9600);
-//defining the interupt for the rising edge of the square wave signal.
-attachInterrupt(digitalPinToInterrupt(2), nullify, RISING);
+	Serial.begin(115200);
+	Serial.println("Frequency Counter");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
-  //Samples the input on pin 2 and adds 1 to either high or low variable.
-  pinread = digitalRead(2);
-  digitalWrite(3, pinread);
-  Serial.println("input is: " + String(pinread));
-  
-  if (pinread == HIGH){
-    highsig++;
-  }
-    
-  if (pinread == LOW){
-    lowsig++;
-  }
-  //This section essentialy counts the run time of the program.  
-  currenttime = micros();
-  
-  t = currenttime - previoustime;
-  previoustime = currenttime;
-  
-  //Some prints to help in testing.
-  Serial.println("Program Run time is: " + String(period));
-  Serial.println("High signal value is:" + String(highsig));
-  Serial.println("Low signal value is:" + String(lowsig));
+	// stop the timer interrupts from throwing the count out
+	byte oldTCCR0A = TCCR0A;
+	byte oldTCCR0B = TCCR0B;
+	TCCR0A = 0;    // stop timer
+	TCCR0B = 0;
 
+	count(500);  // how many ms to count for
+
+	// wait until counting is done
+	while (!counterReady) {}
+
+	// adjust counts by counting interval to give frequency in Hz
+	float frq = (timerCounts *  1000.0) / timerPeriod;
+
+	// print data
+	Serial.print("Frequency: ");
+	Serial.print((unsigned long)frq);
+	Serial.println(" Hz.");
+
+	// restart timer
+	TCCR0A = oldTCCR0A;
+	TCCR0B = oldTCCR0B;
+
+	// let serial stuff finish
+	delay(200);
 }
-void nullify() {
-  //When we encounter a rising edge, this is supposed to calculate the period of the square wave and reset the variables.
-  
-  period = (highsig + lowsig) * t;
-  highsig = 0;
-  lowsig = 0;
 
-}
+void count(unsigned int ms)
+{
+	counterReady = false;         // time not up yet
+	timerPeriod = ms;             // how many 1 ms counts to do
+	timerTicks = 0;               // reset interrupt counter
+	overflowCount = 0;            // no overflows yet
+
+								  // reset Timer 1 and Timer 2
+	TCCR1A = 0;
+	TCCR1B = 0;
+	TCCR2A = 0;
+	TCCR2B = 0;
+
+	// Timer 1 - counts events on pin D5
+	TIMSK1 = bit(TOIE1);   // interrupt on Timer 1 overflow
+
+						   // Timer 2 - gives us our 1 ms counting interval
+						   // 16 MHz clock (62.5 ns per tick) - prescaled by 128
+						   //  counter increments every 8 µs. 
+						   // So we count 125 of them, giving exactly 1000 µs (1 ms)
+	TCCR2A = bit(WGM21);   // CTC mode
+	OCR2A = 124;            // count up to 125  (zero relative!!!!)
+
+							// Timer 2 - interrupt on match (ie. every 1 ms)
+	TIMSK2 = bit(OCIE2A);   // enable Timer2 Interrupt
+
+	TCNT1 = 0;      // Both counters to zero
+	TCNT2 = 0;
+
+	// Reset prescalers
+	GTCCR = bit(PSRASY);        // reset prescaler now
+								// start Timer 2
+	TCCR2B = bit(CS20) | bit(CS22);  // prescaler of 128
+									 // start Timer 1
+									 // External clock source on T1 pin (D5). Clock on rising edge.
+	TCCR1B = bit(CS10) | bit(CS11) | bit(CS12);
+}  // end of startCounting
+
+ISR(TIMER1_OVF_vect)
+{
+	++overflowCount;               // count number of Counter1 overflows  
+}  // end of TIMER1_OVF_vect
+
+
+   //******************************************************************
+   //  Timer2 Interrupt Service is invoked by hardware Timer 2 every 1 ms = 1000 Hz
+   //  16Mhz / 128 / 125 = 1000 Hz
+
+ISR(TIMER2_COMPA_vect)
+{
+	// grab counter value before it changes any more
+	unsigned int timer1CounterValue;
+	timer1CounterValue = TCNT1;  // see datasheet, page 117 (accessing 16-bit registers)
+	unsigned long overflowCopy = overflowCount;
+
+	// see if we have reached timing period
+	if (++timerTicks < timerPeriod)
+		return;  // not yet
+
+				 // if just missed an overflow
+	if ((TIFR1 & bit(TOV1)) && timer1CounterValue < 256)
+		overflowCopy++;
+
+	// end of gate time, measurement ready
+
+	TCCR1A = 0;    // stop timer 1
+	TCCR1B = 0;
+
+	TCCR2A = 0;    // stop timer 2
+	TCCR2B = 0;
+
+	TIMSK1 = 0;    // disable Timer1 Interrupt
+	TIMSK2 = 0;    // disable Timer2 Interrupt
+
+				   // calculate total count
+	timerCounts = (overflowCopy << 16) + timer1CounterValue;  // each overflow is 65536 more
+	counterReady = true;              // set global flag for end count period
+}  // end of TIMER2_COMPA_vect
 
 
